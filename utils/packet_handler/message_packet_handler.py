4ee798264e034623f2e87be584ac52f3749d1a75
HDDTHR <stevenjildeh@gmail.com>
2024-07-07 23:22:30 +0300

    Add execute method to message handler

--- utils/packet_handler/message_packet_handler.py
+++ utils/packet_handler/message_packet_handler.py
@@ -1,13 +1,16 @@
+import logging
 from typing import override
 from utils.flatb.DMessage import DMessage
 from utils.base_packet_handler import BasePacketHandler
 
-
 class MessagePacketHandler(BasePacketHandler):
+    def __init__(self):
+        super().__init__(DMessage)
+
     @override
     def _identifier():
         return 0x0001
-
-    def __init__(self):
-        super().__init__(DMessage)
     
+    @override
+    def execute(self):
+        logging.info(f"Message - {self._data.Text()}") # type: ignore *pain in the ass to add type*

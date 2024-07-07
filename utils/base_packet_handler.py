73bdb275a081643fec3543812e6259eb041ec709
HDDTHR <stevenjildeh@gmail.com>
2024-07-07 23:20:56 +0300

    Fix BasePacketHandler typing

--- utils/base_packet_handler.py
+++ utils/base_packet_handler.py
@@ -1,7 +1,9 @@
 import socket
 import struct
-from typing import Any, final, Type
+from typing import Any, Optional, TypeVar, final, Type
 from abc import ABC, abstractmethod
+
+from utils.flatb_object import HasGetRootAs
 
 class BasePacketHandler(ABC):
     @staticmethod
@@ -9,15 +11,21 @@ class BasePacketHandler(ABC):
     def _identifier():
         pass
 
-    def __init__(self, data_class: Type[Any]):
-        self.__data = None
-        self.__data_class = data_class
+    @abstractmethod
+    def execute(self):
+        pass
+
+    def __init__(self, data_class: Type['T']):
+        self._data : Optional[T] = None
+        self.__data_class : Type[T] = data_class
     
     def handle_socket(self, sock: socket.socket) -> Any:
         payload_size = struct.unpack("I", sock.recv(4))[0]
         raw_data = sock.recv(payload_size)
-        return self.__data_class.GetRootAs(raw_data)
+        self._data =  self.__data_class.GetRootAs(raw_data, 0)
 
     @final
     def receive(self, sock: socket.socket) -> None:
         self.handle_socket(sock)
+
+T = TypeVar('T', bound=HasGetRootAs)

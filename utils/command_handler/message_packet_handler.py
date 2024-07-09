9582840c4b0d6d6054d6c485a2de6bd3886cca27
HDDTHR <stevenjildeh@gmail.com>
2024-07-09 21:03:52 +0300

    Remove redundant package

--- utils/command_handler/message_packet_handler.py
+++
@@ -1,9 +0,0 @@
-from socket import socket
-from utils.flatb.d_message import d_message
-from utils.base_packet_handler import BasePacketHandler
-
-
-class MessagePacketHandler(BasePacketHandler):
-    def __init__(self):
-        super().__init__(0x01, d_message)
-    
